e132b55fc7ad7fb854bd002be7e89f6b5c88682b
isom3400 <isom3400@ggmail.ust.hk>
2025-11-21 09:18:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.title("Business Dashboard with Streamlit Layouts")
 msg = "## Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard."
 st.write(msg)
 
+col1, col2, col3 = st.columns(3)
+
