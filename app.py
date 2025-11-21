18f9f4aac3cf8127ccffa3da83ce769bccad9206
isom3400 <isom3400@ggmail.ust.hk>
2025-11-21 10:09:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,14 @@ from numpy.random import default_rng as rng
 df = pd.DataFrame(rng(0).standard_normal((20, 3)), columns=["a", "b", "c"])
 
 st.area_chart(df)
+
+
+
+
+
+option = st.selectbox(
+    "How would you like to be contacted?",
+    ("Email", "Home phone", "Mobile phone"),
+)
+
+st.write("You selected:", option)
