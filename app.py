b1764a7464390bf3c73d9583a3cbf1d63019a96b
isom3400 <isom3400@ggmail.ust.hk>
2025-11-21 09:39:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ with tab2:
     ]
     idx = 1
     for feedback in customer_feedback:
-        st.write(f"{idx} {feedback}")
+        st.write(f"{idx}. {feedback}")
         idx += 1
 with tab3:
     st.write("Content for Market Trends")
