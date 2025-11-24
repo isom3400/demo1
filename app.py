3f4a554f04123b9f8bac18f7db8d22454000c1d7
isom3400 <isom3400@ggmail.ust.hk>
2025-11-24 13:45:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
-
-import pandas as pd
 import streamlit as st
+from streamlit_option_menu import option_menu
 
-# Sample data
-data = {'Product': ['A', 'B', 'C'], 
-        'Sales': [1200, 850, 950], 
-        'Customers': [300, 400, 350]}
-df = pd.DataFrame(data)
+st.title('Hello, Students!')
+st.write('This is your Python Programming course.')
 
-# Show data with Streamlit elements
-st.dataframe(df)                # Interactive table
-st.data_editor(df)              # Editable table
-st.table(df)                    # Static table
+with st.sidebar:
+    selected=option_menu(
+        menu_title = "Menu",
+        options = ["Home", "About", "Contact"],
+        icons = ["1-circle-fill",
+                 "2-circle-fill",
+                 "3-circle-fill"],
+        menu_icon= "emoji-smile-fill",
+        default_index=0,
+    )
 
-# Customize columns directly in the dataframe display
-st.dataframe(df.style.format({'Sales': '${:,.0f}', 'Customers': '{:,.0f}'}))
+if selected == "Home":
+    st.title(f"Welcome to the {selected} page.")
+
+if selected == "About":
+    st.title(f"Welcome to the {selected} page.")
+
+if selected == "Contact":
+    st.title(f"Welcome to the {selected} page.")
